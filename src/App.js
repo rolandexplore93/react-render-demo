7afb6e6d7c5da035993d62177961254f33ab6b97
rolandexplore93 <roland2rule@gmail.com>
2022-04-13 19:35:53 +0100

    Set up a <UseState /> with an increment count button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UseState from './Components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      Learning React Render
+      <h1>Learning React Render</h1>
+      <UseState/>
     </div>
   );
 }
